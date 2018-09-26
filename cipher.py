22038e23c3a46cef8195771d4b303f78dd53427b
Eli <eliarbogast@gmail.com>
2018-09-26 14:11:55 -0500

    Initial commit

---
+++ cipher.py
@@ -0,0 +1,12 @@
+'''
+Cipher Project: Takes user input message and password and encrypts it using a "Vignère Cipher"
+CS 111, Fall 2018
+date: October 2nd, 2018
+Names: Eli Arbogast and Drew Garcia
+'''
+
+def main():
+
+
+
+main()
