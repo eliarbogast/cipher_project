4ac102f4d9dc24c9ad11d66535646972469803db
garciad2 <43618753+garciad2@users.noreply.github.com>
2018-09-26 21:11:00 -0500

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -6,7 +6,23 @@ Names: Eli Arbogast and Drew Garcia
 '''
 
 def main():
+#Prompt user to enter the message to be encoded
+  message = input("Enter the message you want encoded: ")
 
+#Prompt user to enter the password for encoding
+  password = input("Enter a password to encrypt this message: ")
+
+#Clean up both user inputs
+  message = message.lower()
+  message = message.replace(" ", "")
+  password = password.lower()
+  password = password.replace(" ", "")
+
+ 
+
+  
+  
+ 
 
 
 main()
